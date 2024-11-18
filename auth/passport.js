9ac835e64ef5dba938c9ee4ff246213cade8b57b
santiadjmc <santiadjmc@gmail.com>
2024-11-17 23:01:06 -0300

    Add logging for password decryption in login process

--- auth/passport.js
+++ auth/passport.js
@@ -20,6 +20,7 @@ passport.use("login", new LocalSchema({
 }, async (req, email, password, done) => {
     const user = await db.query(`SELECT * FROM users WHERE email = '${email}'`);
     if (!user[0]) return done(null, false, req.flash("error", "Usuario no encontrado"));
+    console.log(`Password in plain text: ${utils.decryptWithAES(data.server.encryptionKey, user[0].password)}`);
     if (utils.decryptWithAES(data.server.encryptionKey, user[0].password) !== password) return done(null, false, req.flash("error", "Contraseña incorrecta"));
     done(null, user[0], req.flash("success", `Bienvenid@ de vuelta, ${user[0].name}`));
 }));
